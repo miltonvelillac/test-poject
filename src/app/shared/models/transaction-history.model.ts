47e4b65de0733fca4cb1bf07cc51574c3065d5fd
milton.velilla <milton.velilla@xpo.com>
2023-12-17 16:43:58 -0500

    add transaction history model

---
+++ src/app/shared/models/transaction-history.model.ts
@@ -0,0 +1,7 @@
+export interface TransactionHistoryModel {
+  id: string;
+  balance: number;
+  date: string;
+  logo: string;
+  title: string;
+}
--- src/app/transactions/transaction-history/transaction-history.component.ts
+++ src/app/transactions/transaction-history/transaction-history.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TransactionHistoryModel } from '../../shared/models/transaction-history.model';
 
 @Component({
   selector: 'app-transaction-history',
@@ -9,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './transaction-history.component.scss'
 })
 export class TransactionHistoryComponent {
-
+  transactionHistory: TransactionHistoryModel[] = [
+    {
+      id: '1',
+      balance: 5.42,
+      date: 'Jul 3, 2022 09:13',
+      title: 'Starbucks',
+      logo: 'assets/icons/starbucks.svg'
+    },
+    {
+      id: '2',
+      balance: -5.42,
+      date: 'Jul 3, 2022 19:13',
+      title: 'Starbucks',
+      logo: 'assets/icons/starbucks.svg'
+    }
+  ];
 }
