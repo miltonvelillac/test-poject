7479f7df7b2ac2575912bf270eb8dbccf3070fc7
milton.velilla <milton.velilla@xpo.com>
2023-12-17 16:25:00 -0500

    add transaction-history component

---
+++ src/app/transactions/transaction-history/transaction-history.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-history',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transaction-history.component.html',
+  styleUrl: './transaction-history.component.scss'
+})
+export class TransactionHistoryComponent {
+
+}
