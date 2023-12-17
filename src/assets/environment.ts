103a6c42ddb7954398362371b8e59fa3c8cd327d
milton.velilla <milton.velilla@xpo.com>
2023-12-17 18:40:09 -0500

    add firebase config

---
+++ src/assets/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyD3Xhyu8LV05j6C8ANyIZFkBLLndhxKsos",
+    authDomain: "charlie-test-d97eb.firebaseapp.com",
+    projectId: "charlie-test-d97eb",
+    storageBucket: "charlie-test-d97eb.appspot.com",
+    messagingSenderId: "318648965403",
+    appId: "1:318648965403:web:a5ed0962c9df26f7d85bbe"
+  },
+};
